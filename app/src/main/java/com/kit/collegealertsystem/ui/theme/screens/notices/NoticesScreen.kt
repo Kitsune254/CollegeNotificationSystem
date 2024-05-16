3db400bcf27cf01f7b1486d5396ac787d6851a4d
Kitsune254 <owokoselwyn@gmail.com>
2024-05-16 09:12:04 +0300

    added files

--- app/src/main/java/com/kit/collegealertsystem/ui/theme/screens/notices/NoticesScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.kit.collegealertsystem.ui.theme.screens.notices
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-
-@Composable
-fun NoticesScreen(navController: NavHostController) {
-    
-}
